352bb9f259558ebf5fdc81c23b8528328c17b31c
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2023-01-23 14:54:13 +0800

    Add bsi

---
+++ bsi/cache.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <memory>
+#include <vector>
+#include "roaring/roaring.h"
+namespace bsi {
+
+roaring_bitmap_t* acquire_bitmap();
+void release_bitmap(roaring_bitmap_t* b);
+
+void clear_bitmap_cache();
+
+class BitMapCacheGuard
+{
+private:
+    std::vector<roaring_bitmap_t*> cache_;
+
+public:
+    void Add(roaring_bitmap_t* b);
+    ~BitMapCacheGuard();
+};
+
+}  // namespace bsi
